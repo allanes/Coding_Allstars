ef68e8900a295d25a3d40e22fa58a45459f71f22
Adrian Llanes <ados.adrian@gmail.com>
2023-02-16 12:07:31 -0300

    added railway.json for deployment test

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
-async def read_html():
+def read_html():
     TARGET_LANGUAGE = 'es'
     with open(f"website-A_translated_{TARGET_LANGUAGE}.html", "r", encoding='utf-8') as file:
         html_content = file.read()
     return html_content
+
