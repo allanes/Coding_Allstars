6fa6de0ef1a35e0a284851251b40fa0e0500b049
Adrian Llanes <ados.adrian@gmail.com>
2023-02-16 11:50:15 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_html():
+    TARGET_LANGUAGE = 'es'
+    with open(f"website-A_translated_{TARGET_LANGUAGE}.html", "r", encoding='utf-8') as file:
+        html_content = file.read()
+    return html_content
